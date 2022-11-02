1449c65061952949c5c875d4b4635ccfc79a853f
Kaporos <theo@daron.be>
2022-11-02 13:31:05 +0100

    Removed useless imports

--- simulation.py
+++ simulation.py
@@ -1,7 +1,3 @@
-from matplotlib.backends.backend_tkagg import (
-    FigureCanvasTkAgg, NavigationToolbar2Tk)
-# Implement the default Matplotlib key bindings.
-from matplotlib.backend_bases import key_press_handler
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -15,7 +11,7 @@ class Simulation:
         self.k = k
         self.e = e # [Joules]
         self.m = m # [Kilos]
-        self.w = 0.01
+        self.dt = 0.01
 
         self.__figures = []
     def figures(self):
@@ -25,6 +21,6 @@ class Simulation:
         self.__figures = []
         fig, axes = plt.subplots(2, 2, constrained_layout=True)
         axes[0][0].set_title("Speed !")
-        t = np.arange(0, np.pi * 2, self.w) #[0,0.01,0.02,0.03,....3]
+        t = np.arange(0, np.pi * 2, self.dt) #[0,0.01,0.02,0.03,....3]
         axes[0][0].plot(t,  np.sin(t))
         self.__figures.append(fig) 
